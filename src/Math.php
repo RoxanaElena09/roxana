be36da3a6bfc379eadf5040cf90838e0968450cb
RoxanaElena09 <popescuroxana09@yahoo.ro>
2025-10-27 12:02:14 +0100

    fix: met à jour phpunit.xml et la classe Math

--- src/Math.php
+++ src/Math.php
@@ -5,14 +5,28 @@ namespace App;
 
 final class Math
 {
-    public function add(float|int $a, float|int $b): float { return $a + $b; }
-    public function sub(float|int $a, float|int $b): float { return $a - $b; }
-    public function mul(float|int $a, float|int $b): float { return $a * $b; }
+    public function add(float|int $a, float|int $b): float
+    {
+        return $a + $b;
+    }
+
+    public function sub(float|int $a, float|int $b): float
+    {
+        return $a - $b;
+    }
+
+    public function mul(float|int $a, float|int $b): float
+    {
+        return $a * $b;
+    }
+
     public function div(float|int $a, float|int $b): float
     {
         if ($b == 0.0) {
             throw new \InvalidArgumentException('Division by zero');
         }
+
         return $a / $b;
     }
 }
+
